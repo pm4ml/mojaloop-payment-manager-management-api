d97e89543d04dc9527951d31c48b138d8bf08374
Yevhen Kyriukha <kirgene@gmail.com>
2022-07-04 19:29:25 +0300

    fix constrol server message broadcasting.

--- src/ControlServer/Server.ts
+++ src/ControlServer/Server.ts
@@ -153,7 +153,11 @@ class Server extends ws.Server {
 
   // Close the server then wait for all the client sockets to close
   async stop() {
-    await new Promise(this.close.bind(this));
+    const closing = new Promise((resolve) => this.close(resolve));
+    for (const client of this.clients) {
+      client.terminate();
+    }
+    await closing;
     this._logger.log('Control server shutdown complete');
   }
 
@@ -192,35 +196,28 @@ class Server extends ws.Server {
    * from other modules.
    */
   registerInternalEvents() {
-    ControlServerEventEmitter.on(INTERNAL_EVENTS.SERVER.BROADCAST_CONFIG_CHANGE, (params) => {
-      this.broadcastConfigChange(params);
-    });
+    ControlServerEventEmitter.on(INTERNAL_EVENTS.SERVER.BROADCAST_CONFIG_CHANGE, (params) => this.broadcastConfigChange(params));
   }
 
   /**
    * Broadcast configuration change to all connected clients.
    */
-  async broadcastConfigChange(updatedConfig) {
+  broadcastConfigChange(updatedConfig) {
     const updateConfMsg = build.CONFIGURATION.NOTIFY(updatedConfig, randomPhrase());
-    const errorLogger = (socket, message) => (err) =>
-      this._logger
-        .push({ message, ip: this._clientData.get(socket).ip, err })
-        .log('Error sending JWS keys notification to client');
-    return this.broadcast(updateConfMsg, errorLogger);
+    return this.broadcast(updateConfMsg);
   }
 
   /**
    * Broadcasts a protocol message to all connected clients.
    *
    * @param {string} msg
-   * @param {object} errorLogger
    */
-  async broadcast(msg: string, errorLogger) {
-    return Promise.all(
-      [...this.clients.values()].map((socket) =>
-        new Promise((resolve) => socket.send(msg, resolve)).catch(errorLogger(socket, msg))
-      )
-    );
+  broadcast(msg: string) {
+    this.clients.forEach((client) => {
+      if (client.readyState === ws.WebSocket.OPEN) {
+        client.send(msg);
+      }
+    });
   }
 }
 
