85ebdfe7ddbceb57698846aa1a8e1b15e6cdd767
Akku-Rawat <akhileshrawat.asr@gmail.com>
2025-01-06 12:52:46 +0530

    chore: update Jest configuration
    - Configured code coverage settings and thresholds

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
   moduleNameMapper: {
     '^@app/(.*)$': '<rootDir>/src/$1',
   },
@@ -13,4 +12,27 @@ module.exports = {
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.test.json' }],
   },
+  testEnvironment: 'jest-environment-node',
+
+  // Configure reporters for test results
+  reporters: ['default', ['jest-junit', { outputDirectory: 'reports', outputName: 'report.xml' }]],
+
+  // Automatically clear mock calls and instances between tests
+  clearMocks: true,
+
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['json', 'text', 'lcov', 'text-summary'],
+
+  // An object that configures minimum threshold enforcement for coverage results
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      functions: 90,
+      branches: 90,
+      lines: 90,
+    },
+  },
+
 };
