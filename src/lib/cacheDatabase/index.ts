87104a94d7b4d0565d2736bdad73222b8b311508
Kevin Leyow <kleyow@gmail.com>
2025-04-03 19:40:01 -0500

    chore: changes

--- src/lib/cacheDatabase/index.ts
+++ src/lib/cacheDatabase/index.ts
@@ -98,14 +98,14 @@ async function syncDB({ redisCache, db, logger }: SyncDBOpts) {
         try {
           data.quoteResponse.body = JSON.parse(data.quoteResponse.body);
         } catch (err) {
-          logger.push({ err }).log('Error parsing JSON quoteResponse body');
+          logger.push({ err }).log('Error parsing quoteResponse body');
         }
       }
       if (data.fulfil?.body) {
         try {
           data.fulfil.body = JSON.parse(data.fulfil.body);
         } catch (err) {
-          logger.push({ err }).log('Error parsing JSON fulfil body');
+          logger.push({ err }).log('Error parsing fulfil body');
         }
       }
     }
